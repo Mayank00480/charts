8a9079b7e29bf0ee16f34ceec035b7ca5be802fd
Mayank <mayank@bipolarfactory.com>
2025-07-18 12:21:57 +0530

    refactor: remove unused MyButton and Counter components from exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
-import { MyButton } from './MyButton';
-import { Counter } from './Counter';
+
 import { WalkinTrafficChart } from './WalkinTrafficChart';
 import FootFallHeatMap from './FootFallHeatMap';
 import TopStoresChart from './TopPerformanceStore';
-export {MyButton , Counter  , WalkinTrafficChart ,FootFallHeatMap , TopStoresChart}
+export { WalkinTrafficChart ,FootFallHeatMap , TopStoresChart}
